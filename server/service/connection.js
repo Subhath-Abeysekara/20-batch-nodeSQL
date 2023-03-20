7a0eed3adff2d4a27f3e546fee820c51b9a9738f
Subhath Abeysekara <abeysekarasdp.19@uom.lk>
2023-03-20 15:36:34 +0530

    change db and intergrated api functions

--- server/service/connection.js
+++ server/service/connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user:"sqluser",
-  password:"password",
-  database : "database"
+  host: "ezpark-db.cvhbqqtsx1je.ap-northeast-1.rds.amazonaws.com",
+  user:"admin",
+  password:"ezPark!123",
+  database : "EzPark"
 });
 connection.connect(function(err) {
     if (err) throw err;
