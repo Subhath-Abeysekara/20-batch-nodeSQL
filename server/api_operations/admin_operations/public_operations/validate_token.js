806e9b7b931df1c24dc872b1637a3e127f2a12d9
Subhath Abeysekara <92707080+Subhath-Abeysekara@users.noreply.github.com>
2023-03-29 09:47:33 +0530

    Update validate_token.js

--- server/api_operations/admin_operations/public_operations/validate_token.js
+++ server/api_operations/admin_operations/public_operations/validate_token.js
@@ -4,7 +4,7 @@ var validate_token = require('../../../authentication/authenticate')
 module.exports = async function validate(req , res){
     var validity = await validate_token(req , 3)
     console.log("validity2 ",validity)
-    if(validity){
+    if(validity.condition){
         res.send("valid")
     }
     else{
